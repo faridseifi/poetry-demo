88ff54d9b4b0c97a209bbb45c66884c926fc1177
Farid Seifi <faridsei@med.umich.edu>
2023-06-12 12:04:02 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    description='Hello world',
+    version='0.1',
+    url='http://github.com/example/linode_example',
+    author='Farid',
+    author_email='farid.seifi@gmail.com',
+    keywords=['test']
+    )
